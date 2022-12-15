939b6d6b955e0f978675b46f133521838140d1d8
Ashish Chawla <ashishmchawla@gmail.com>
2022-12-15 15:38:59 +0530

    Completion of Redux Setup

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -2,6 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    _id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    notifications: true,
+    tokens: [],
+  },
+  reducers: {
+    saveUser: (state, payload: any) => {
+      state._id = payload._id;
+      state.firstName = payload.firstName;
+      state.lastName = payload.lastName;
+      state.email = payload.email;
+      state.notifications = payload.notifications;
+      state.tokens = payload.tokens;
+    },
+    updateTokens: (state, payload: any) => {
+      state.tokens = payload.tokens;
+    },
+  },
 });
+
+export const { saveUser, updateTokens } = userSlice.actions;
+export default userSlice.reducer;
